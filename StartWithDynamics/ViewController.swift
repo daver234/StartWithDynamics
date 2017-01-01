3a0f637ff993a6be2b8918e2bb2c40c5ad604dd1
daver234 <dave.rothschild@gmail.com>
2016-12-31 20:31:47 -0800

    Initial Commit

---
+++ StartWithDynamics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartWithDynamics
+//
+//  Created by David Rothschild on 12/31/16.
+//  Copyright © 2016 Dave Rothschild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
